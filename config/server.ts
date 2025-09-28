effc5634968d79d5fc114e02298ff5e27863dd7b
Toro <anmolmm04@gmail.com>
2025-09-28 23:10:26 +0530

    deployment testing part 7

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,12 @@
+// path: ./config/server.js
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', ''), // This is the crucial line
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
